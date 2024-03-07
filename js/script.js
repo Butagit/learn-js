dc180da6f3cb6778ec189d3fa295b0e91f793433
dima <2005Dimas0112@gmail.com>
2024-03-07 17:24:58 +0300

    info: scripts and time for execution, setTimaout and setInterval

--- js/script.js
+++ js/script.js
@@ -1,34 +1,73 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tabheader__item');
-    const tabsContent = document.querySelectorAll('.tabcontent');
-    const tabsParent = document.querySelector('.tabheader__items');
+// const timer = setTimeout(function() {
+    //     console.log('hi');
+    // }, 2000);
+    
+    // такой способ используеться редко
+    // const timer = setTimeout(function(text) {
+    //     console.log(text);
+    // }, 2000, 'okey');
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
+const btn = document.querySelector('.btn');
+let timerId;
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-    function showTabContent(i = 0) { //устанавливаеться по умолчанию если при вызове функции ничего не передаеться
-        tabsContent[i].style.display = 'block';
-        item.classList.remove('tabheader__item_active');
+let i = 0;
 
-    };
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+    const id = setInterval(frame, 10);
 
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-
-                }
-            });
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
         }
-    });
+    }
 
-});
+}
+
+
+
+btn.addEventListener('click', myAnimation);
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000); //тут функцию вызывать не нужно, мы передаем ссылку
+//     timerId = setInterval(logger, 500); // сначало идет задержка 500 потом идет logger который прогружаеться 3 секунду потом задержка и опять logger и потом интервал не будет опять ждать 500, он думат что эти 500 уже прошли во время выполнение этой функции и он сразу же начнет запускать функцию logger
+// });
+
+// // setTimeout(logger, 2000); //может работать без назначение переменной, но лучше работать с потому что так мы сможем ее определить и в случае чего мы могли ее остановить
+
+// // clearInterval(timerId); // таким способ мы остановили setTimeout и теперь в консоль ничего ничего не булет выводиться 
+
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(timerId); 
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// // РЕКУРСИВНЫЙ setTimeout , то есть окод всегда будет ждать строго отведенное ему время
+
+// let id = setTimeout(function log() {
+//     console.log('hi');
+//     id = setTimeout(log, 500);
+// }, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
