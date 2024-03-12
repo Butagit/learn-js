d697f166f1449a12e605c49c5c82c22b120198ec
dima <2005Dimas0112@gmail.com>
2024-03-12 22:34:02 +0300

    info: This

--- js/script.js
+++ js/script.js
@@ -1,42 +1,99 @@
-// const now = new Date('2020-12-4'); // только тут это происходит автоматически
-//     new Date.parse('2020-12-4'); // а сдечь мы используем метод но результат одни и тот же
-// const now = new Date(1710000563145);
-// const now = new Date(2024, 3, 9, 20);
-// все измеряется в миллисекундах 
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDay());
-// console.log(now.getDate());
-// console.log(now.getHours());
-// console.log(now.getUTCHours());
-// console.log(now.getTimezoneOffset()); // это разница между часовым поясом и UTC
-// console.log(now.getTime()); //колличество миллисекунд пройденных с 1970 года по сей день
-// console.log(now.setFullYear(2005));
-// console.log(now.setMonth(12));
-// // console.log(now.setDay(4));
-// console.log(now.setDate(1));
-// console.log(now.setHours(18, 40, 53)); 
-// // console.log(now.setHours(18)); // в консоли может показывать на 3 часа раньше но в браузере будет показывать как надо 
-// // так как браузер подстраивается под наш регион
-// преобразование динамичных типов данных и поставить + перед обьектом даты
+'use strict';
+// 'use strict';
 
-// есть еще оператор ** который возводит в степень число
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this)  
+//         return this.a + this.b
+        
+//     }
+// }
 
-// let start = new Date() // берем 1 дату
+// showThis(4, 5);
 
-// for (let i = 0; i < 100000; i++) {
-//     let some = i ** 3;
+// 1) обычная функция: this = window, если use strict = undefined
+
+// const obj = {
+//     a: 5,
+//     b: 8,
+//     show: function() {
+//         console.log(this);
+//         function showThis() {
+//             console.log(this);
+//         }
+
+//         showThis();
+//     }
 // };
 
-// let end = new Date(); // 2 дату 
+// function showThis() {
+//     console.log(this);
+// }
+// showThis();
 
-// alert(`разница составляет ${end - start}`); //  и исчесляем их разницу
+// obj.show()
 
 
+// 2) контекст метода обьекта - сам обьект
+
+// function User (name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log('Hello' + this.name);
+//     };
+// }
+
+// let Ivan = new User('Ivan', 30);
+// Ivan.hello();
+// console.log(Ivan);
+
+// 3) this в конструкторах и классах - это экземпляр обьекта
+
+// function sayName() {
+//     console.log(this);
+//     console.log(this.name + 'Smith');
+
+// }
+
+// const user = {
+//     name: 'John'
+// }
+
+// sayName.call(user, 'Smith' ); //  позволяет связать функцию с обьектом 
+// sayName.apply(user, ['Smith']); // тоже самое только у них разный синтаксис
 
 
+// создает новую функцию и под нее уже подвязывет контекст
 
+// function count(num) {
+//     return this*num;
+// }
 
+// const double = count.bind(2) // в эту переменную мы помещаем новую функцию она должна удваивать любое число
+// console.log(double(3)); 
+// console.log(double(13));  
+
+// 4) ручная привязка this: call, apply, bind
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function () { // если у нас callback функция будет вот такая то this будет сам элемент на котором произошло событие
+//     // console.log(this);
+//     this.style.backgroundColor = 'red';  
+// })
+
+// // в стрелочной функции если она находиться в родителе а родитель это метод (функция) то при вызове this в общем итоге она выводит сам обьект
+// const obj = {
+//     num: 5,
+//     sayNumber: function() {
+//         const say = () => {
+//             console.log(this);
+//         }
+//     }
+// }
 
 
 
