3413c3d9edc90fbd7b4e67fa8cde3da752670e28
dima <2005Dimas0112@gmail.com>
2024-03-28 11:49:06 +0300

    info: rest operator

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
+// rest оператор собирает элементы и обьединяет его в один массив
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+}
 
-const box = document.querySelector('.box')
-const button = document.querySelector("button");
-// const height = box.clientHeight;
-// const width = box.clientWidth;
-// const height = box.offsetHeight;
-// const width = box.offsetWidth;
-// const height = box.scrollHeight;
-// const width = box.scrollWidth;
+log('basic', 'rest', 'operator', 'usage');
 
-button.addEventListener('click', () => {
-    // box.style.height = box.scrollHeight + 'px'; 
-    console.log(box.scrollTop); 
-})
+// параметры по умолчанию
+function calcOrDouble(number, basic = 2) { // можно просто присвоить в basic 2
+    // basic = basic || 2;
+    console.log(number * basic)
+}
 
-console.log(height, width);
+calcOrDouble(3);
 
-
-// получает все координаты которые есть у элемента 
-console.log(box.getBoundingClientRect);
-// если мы смотрит расстояние right то мы должны знать что это считаеться от левой границы до правой границы элемента
-// когда мы смотрим с низу у нас bottom рассчитываеться с верхней гарницы до нижней границы элемента 
-// console.log(box.getBoundingClientRect.top); тут мы получим только значение top
-
-// как получить стили которые уже были применены из css 
-
-const style = window.getComputedStyle(box,);
-
-console.logs(style); //так же через точку мы можем выбрать нужный нам стиль
-// мы не можем просто так работать с псевдо элеметами, но стили псевдо элеметов мы можем получить что бы это сделать нужно прописать сюда как 2 значение 
-// const style = window.getComputedStyle(box,);
-
-console.log(document.documentElement.scrollTop); //для того что юы узнать количество пикселей
